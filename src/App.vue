0f44eaf9af8fa383636504647d6f5b584c082be3
MarioGarcia0211 <jhonmariogarciaballesteros@gmail.com>
2025-07-12 20:43:24 -0500

    Se creo un loading al momento de recargar la pagina

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <router-view />
+  <div v-if="loadingApp" class="loading-overlay">
+    <div class="spinner-border text-primary" role="status"></div>
+  </div>
+
+  <router-view v-else />
 </template>
 
 <script>
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/config"; // Ajusta la ruta si es diferente
+
 export default {
+  data() {
+    return {
+      loadingApp: true,
+    };
+  },
   watch: {
     $route(to) {
       this.setBodyClass(to.path);
@@ -11,6 +23,7 @@ export default {
   },
   mounted() {
     this.setBodyClass(this.$route.path);
+    this.inicializarApp();
   },
   methods: {
     setBodyClass(path) {
@@ -18,12 +31,36 @@ export default {
 
       if (path === "/login" || path === "/register") {
         document.body.classList.add("bg-login");
-      } else if (path === "/") {
-        document.body.classList.add("bg-default");
       } else {
         document.body.classList.add("bg-default");
       }
     },
+
+    async inicializarApp() {
+      return new Promise((resolve) => {
+        // Espera a que Firebase detecte si hay usuario autenticado
+        onAuthStateChanged(auth, (user) => {
+          // Puedes hacer algo con el usuario si lo necesitas
+          this.loadingApp = false;
+          resolve();
+        });
+      });
+    },
   },
 };
 </script>
+
+<style scoped>
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  z-index: 9999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
